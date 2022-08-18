baf8cd6cab6bf3b43421bbe2a6c752edd2e50089
TheArcus02 <m.urbaniak.inf@gmail.com>
2022-08-18 16:51:35 +0200

    resolved navbar bug with showing menu options many times

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Typography, Box, IconButton, Menu, MenuItem, Button, Tooltip, Avatar, Link as MuiLink, Badge} from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, IconButton, Menu, MenuItem, Button, Tooltip, Avatar, Link as MuiLink, Badge } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -8,7 +8,7 @@ import slugify from 'react-slugify';
 import { Edit, Logout, NotificationsOutlined } from '@mui/icons-material';
 
 
-export const Navbar:React.FC = () => {
+export const Navbar: React.FC = () => {
 
   const initialPages = ['Offers', 'Pricing', 'Blog'];
   const initialMenu = ['Profile', 'Account']
@@ -21,26 +21,28 @@ export const Navbar:React.FC = () => {
 
   const { currentUser, userInfo, logout } = useAuth() as AuthContextItf
 
-  console.log({currentUser, userInfo});
+  console.log({ currentUser, userInfo });
 
   useEffect(() => {
-    if(userInfo){
-      // TODO handle showing many times solution with return probalby
-      // if(menu.includes('Your Companies') ||
-      //  menu.includes('Create Company') || 
-      //  menu.includes('Your Job Applications')) return //! delete on prod
+    if (userInfo) {
+      if (!menu.includes('Your Job Applications')) {
+        userInfo.jobApplications.length > 0 &&
+          setMenu((prev) => ([...prev, 'Your Job Applications']))
+      }
 
-      userInfo.jobApplications.length > 0 && 
-      setMenu((prev) => ([...prev, 'Your Job Applications']))
+      if (!menu.includes('Your Companies') &&
+        !menu.includes('Create Company')) {
 
-      userInfo.companies.length > 0 ?
-      setMenu((prev) => ([...prev, 'Your Companies', 'Your Job Offers'])) :
-      setMenu((prev) => ([...prev, 'Create Company']))
+        userInfo.companies.length > 0 ?
+          setMenu((prev) => ([...prev, 'Your Companies', 'Your Job Offers'])) :
+          setMenu((prev) => ([...prev, 'Create Company']))
+      }
 
-      
+
+
     }
   }, [userInfo])
-  
+
 
   const handleOpenNavMenu = (event: any) => {
     setAnchorElNav(event.currentTarget);
@@ -60,148 +62,148 @@ export const Navbar:React.FC = () => {
 
   return (
     <AppBar position="static" enableColorOnDark color="primary">
-        <Toolbar>
-          <MuiLink component={Link} to="/" underline="none" color="inherit">
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-            >
-              Work At Home
-            </Typography>
-          </MuiLink>
-          
+      <Toolbar>
+        <MuiLink component={Link} to="/" underline="none" color="inherit">
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            Work At Home
+          </Typography>
+        </MuiLink>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={() => setAnchorElNav(null)}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={() => setAnchorElNav(null)}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-            <Typography
-              variant="h6"
-              noWrap
-              component={Link}
-              to="/"
-              color="inherit"
-              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', textDecoration: 'none' } }}
-            >
-              Work At Home
-            </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="menu"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={() => setAnchorElNav(null)}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
             {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => setAnchorElNav(null)}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
+              <MenuItem key={page} onClick={() => setAnchorElNav(null)}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
             ))}
-          </Box>
-          <Box sx={{display:{xs:"none", md:'inherit'}, mr:3}}>
-            <MuiLink component={Link} to="/add-offer" underline='none' color="inherit">
-              <Button color="secondary" variant='contained' size="small" startIcon={<Edit />}>
-                Post a job
-              </Button>
-            </MuiLink>
-          </Box>
+          </Menu>
+        </Box>
+        <Typography
+          variant="h6"
+          noWrap
+          component={Link}
+          to="/"
+          color="inherit"
+          sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none', textDecoration: 'none' } }}
+        >
+          Work At Home
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'center' }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={() => setAnchorElNav(null)}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+        <Box sx={{ display: { xs: "none", md: 'inherit' }, mr: 3 }}>
+          <MuiLink component={Link} to="/add-offer" underline='none' color="inherit">
+            <Button color="secondary" variant='contained' size="small" startIcon={<Edit />}>
+              Post a job
+            </Button>
+          </MuiLink>
+        </Box>
 
-          <Box sx={{display:{xs:"none", md:'inherit'}, mr:3}}>
-            <Tooltip title="Notifications">
-              <IconButton>
-                <Badge badgeContent={null} color="secondary">
-                  <NotificationsOutlined />
-                </Badge>
-              </IconButton>
-            </Tooltip>
-          </Box>
-          {/* // TODO integrate with Notifications system when ready */}
-          <Box sx={{ flexGrow: 0 }}>
-            {currentUser && userInfo ? (
-              <>
-                <Tooltip title="Open menu">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Profile Picture" src={userInfo.photoUrl ? userInfo.photoUrl : ""} sx={{bgcolor:'#fff'}}>
-                      {!userInfo.photoUrl && currentUser.displayName?.slice(0,2)}
-                    </Avatar>
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: '45px' }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={() => setAnchorElUser(null)}
-                >
-                  {menu.map((menuItem) => (
-                    <MuiLink 
-                      key={menuItem} 
-                      component={Link} 
-                      to={menuItem === 'Profile' ? slugify(menuItem.toLowerCase()) + '/' + currentUser.uid : slugify(menuItem.toLowerCase())} 
-                      textAlign="center" 
-                      underline='none' 
-                      color="inherit"
-                    >
-                      <MenuItem onClick={() => setAnchorElUser(null)}>
-                        {menuItem}
-                      </MenuItem>
-                    </MuiLink>
-                  ))}
-                  <MenuItem onClick={() => handleLogut()}>
-                      <Logout />
-                      <Typography ml={1} textAlign="center">Logout</Typography>
-                  </MenuItem>
-                </Menu>
-              </>
-            ) : (
-              <MuiLink component={Link} to="/login" underline='none' color="inherit">
-                <Button color="inherit" variant="outlined">Login</Button>
-              </MuiLink>
-            )}
-            
-          </Box>
-        </Toolbar>
+        <Box sx={{ display: { xs: "none", md: 'inherit' }, mr: 3 }}>
+          <Tooltip title="Notifications">
+            <IconButton>
+              <Badge badgeContent={null} color="secondary">
+                <NotificationsOutlined />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+        </Box>
+        {/* // TODO integrate with Notifications system when ready */}
+        <Box sx={{ flexGrow: 0 }}>
+          {currentUser && userInfo ? (
+            <>
+              <Tooltip title="Open menu">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Profile Picture" src={userInfo.photoUrl ? userInfo.photoUrl : ""} sx={{ bgcolor: '#fff' }}>
+                    {!userInfo.photoUrl && currentUser.displayName?.slice(0, 2)}
+                  </Avatar>
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={() => setAnchorElUser(null)}
+              >
+                {menu.map((menuItem) => (
+                  <MuiLink
+                    key={menuItem}
+                    component={Link}
+                    to={menuItem === 'Profile' ? slugify(menuItem.toLowerCase()) + '/' + currentUser.uid : slugify(menuItem.toLowerCase())}
+                    textAlign="center"
+                    underline='none'
+                    color="inherit"
+                  >
+                    <MenuItem onClick={() => setAnchorElUser(null)}>
+                      {menuItem}
+                    </MenuItem>
+                  </MuiLink>
+                ))}
+                <MenuItem onClick={() => handleLogut()}>
+                  <Logout />
+                  <Typography ml={1} textAlign="center">Logout</Typography>
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <MuiLink component={Link} to="/login" underline='none' color="inherit">
+              <Button color="inherit" variant="outlined">Login</Button>
+            </MuiLink>
+          )}
+
+        </Box>
+      </Toolbar>
 
     </AppBar>
   );
